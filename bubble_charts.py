b83515d84e91ffe288db3d0255145c6638406079
Dor Levy <dorle@post.bgu.ac.il>
2022-06-29 19:04:08 +0300

    fix bubble chart to fit wix site

--- bubble_charts.py
+++ bubble_charts.py
@@ -2,24 +2,31 @@ import plotly.graph_objects as go
 import chart_studio as cs
 
 def create_bubble_chart():
-    size = [0, 8.620620, 0, 0, 11.228836, 0]
+    size = [8.620620, 11.228836]
     percent_women = 8620620 / (8620620 + 11228836) * 100
     percent_men = 100 - percent_women
     fig = go.Figure(data=[go.Scatter(
-        x=list(range(6)), y=list(range(6)),
-        text=['', 'Total Women:<br>8,620,620 ({per:.2f}%)'.format(per=percent_women),'','', 'Total Men<br>11228836 ({per:.2f}%)'.format(per=(100 - percent_women)), ''],
+        x=['Women', 'Men'], y=list(range(2)),
+        text=['Total Women:<br>8,620,620 ({per:.2f}%)'.format(per=percent_women),'Total Men<br>11,228,836 ({per:.2f}%)'.format(per=(100 - percent_women))],
         mode='markers',
         hoverinfo='text',
         marker=dict(
-            color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)','rgb(255, 65, 54)','rgb(255, 65, 54)','rgb(255, 65, 54)','rgb(255, 65, 54)'],
+            color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)'],
             size=size,
             sizeref = 2. * 11.228836 / (21.228836 ** 2),
             
 
         )
     )])
-    fig.update_xaxes(visible=False)
+
     fig.update_yaxes(visible=False)
+    fig.update_layout(
+    title={
+        'text': "Total Occupation in Cinema",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
     fig.write_html('graphs/total_occupation.html')
     cs.plotly.plot(fig, filename="total_occupation", auto_open=True)
 
